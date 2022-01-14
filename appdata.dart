6bd92d3267e1a18b14ea6d4fdfcf82c1329b19bc
KOWSHIQ KATTAMURI <kattamuri_b201298bt@nitc.ac.in>
2022-01-14 07:55:36 +0530

    Update appdata.dart

--- appdata.dart
+++ appdata.dart
@@ -1,23 +1,51 @@
-import 'package:flutter/material.dart';
-import 'presentation/listpage/list_page.dart';
-import 'presentation/addtodo/add_todo_page.dart';
-import 'package:financialmanagementapp/controller/appdata.dart';
+import 'dart:convert';
+import 'package:financialmanagementapp/models/event.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await AppData.initEvent();
-  runApp(const MyApp());
-}
+// List<Event> globalEventList = [];
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class AppData{
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: const ToDoHomePage(),
-      // home : const AddToDoPage(),
-    );
+ static List<Event> _eventList = [];
+ static late SharedPreferences _sharedPreferences;
+ static List<String> _jsonEventlist = [];
+
+ static Future<void> initEvent() async {
+  _sharedPreferences = await SharedPreferences.getInstance();
+ final _tempJsonEventList = _sharedPreferences.getStringList('todoList');
+ if(_tempJsonEventList != null){
+  _jsonEventlist = _tempJsonEventList;
+  _eventList = _tempJsonEventList.map((e) => Event.fromJson(jsonDecode(e))).toList();
   }
+ }
+
+ static List<Event> getEvents(){
+  return _eventList;
+ }
+
+ static Future<void> addEvent(Event event) async {
+  _eventList.add(event);
+  _jsonEventlist.add(jsonEncode(event));
+  await _sharedPreferences.setStringList('todoList', _jsonEventlist);
+ }
+
+ static Future<void> saveEvents() async {
+  _jsonEventlist = _eventList.map((event) => jsonEncode(event)).toList();
+  await _sharedPreferences.setStringList('todoList', _jsonEventlist);
+ }
+
+ static Future<void> deleteEvent(int index) async{
+  _eventList.removeAt(index);
+  _jsonEventlist.removeAt(index);
+  await _sharedPreferences.setStringList('todoList', _jsonEventlist);
+
+ }
+ static getTotalexpenditure(){
+  int _totalexpenditure = 0;
+
+  for(int i = 0; i < _eventList.length; i++){
+   _totalexpenditure = _totalexpenditure + _eventList[i].itemprice;
+  }
+  return _totalexpenditure;
+ }
 }
